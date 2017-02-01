eeba0ef6ad17fa0fe7d1c950ceb7390d3be8b86c
jasonsouryamath <jasonsouryamath@yahoo.com>
2017-01-31 16:00:47 -0800

    Update AmazonWeb.java

--- src/test/java/aws/AmazonWeb.java
+++ src/test/java/aws/AmazonWeb.java
@@ -53,9 +53,9 @@ public class AmazonWeb {
 	Thread.sleep(1000);
 	new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated
 			(By.cssSelector(".I2B > div:nth-child(2) > button:nth-child(1)"))).click();
-	
+	Thread.sleep(1000);
 	new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated
-			(By.cssSelector("	div.IQ:nth-child(6) > div:nth-child(2) > button:nth-child(1)"))).click();
+			(By.cssSelector("div.IQ:nth-child(6) > div:nth-child(2) > button:nth-child(1)"))).click();
   }
   
   @BeforeClass
